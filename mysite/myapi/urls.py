5cf357d21e4fd24c173fbad89e8ec92dc55e573f
Phuong276 <vuducphuong2001@gmai.com>
2022-11-23 16:59:10 +0700

    feat: api get all books

---
+++ mysite/myapi/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path, re_path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+urlpatterns = [
+
+    path('', include(router.urls)),
+    path('book', views.BookManage.as_view()),
+    
+]
